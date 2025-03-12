70c7791150585ed3ada022f9856cd52026ff5dac
Krittipat Krittakom <160571505+Krittipat-K@users.noreply.github.com>
2025-03-12 18:07:00 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='EVM',
     packages=find_packages(include=['EVM']),
-    version='1.0.0',
+    version='1.0.1',
     description='EVM is a python package for Ethereum Virtual Machine',
     author='Krittipat Krittakom',
     install_requires=['web3',
